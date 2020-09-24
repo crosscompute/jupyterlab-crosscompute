210ad1d7e7e2ddbf32b77d69bf472e96ea3b44cd
Rodrigo Guarachi <rmguarachi@gmail.com>
2020-09-24 20:06:04 +0000

    notebook toolbar button

---
+++ Experiments/toolbar/src/index.ts
@@ -0,0 +1,70 @@
+import {
+  IDisposable, DisposableDelegate
+} from '@lumino/disposable';
+
+import {
+  JupyterFrontEnd, JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import {
+  ToolbarButton
+} from '@jupyterlab/apputils';
+
+import {
+  DocumentRegistry
+} from '@jupyterlab/docregistry';
+
+import {
+  NotebookActions, NotebookPanel, INotebookModel
+} from '@jupyterlab/notebook';
+
+
+/**
+ * The plugin registration information.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  activate,
+  id: 'my-extension-name:buttonPlugin',
+  autoStart: true
+};
+
+
+/**
+ * A notebook widget extension that adds a button to the toolbar.
+ */
+export
+class ButtonExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
+  /**
+   * Create a new extension object.
+   */
+  createNew(panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
+    let callback = () => {
+      NotebookActions.runAll(panel.content, context.sessionContext);
+      console.log('aaaaaaaaa', panel, context)
+    };
+    let button = new ToolbarButton({
+      className: 'myButton',
+      iconClass: 'fa fa-fast-forward',
+      onClick: callback,
+      tooltip: 'Run All'
+    });
+
+    panel.toolbar.insertItem(0, 'runAll', button);
+    return new DisposableDelegate(() => {
+      button.dispose();
+    });
+  }
+}
+
+/**
+ * Activate the extension.
+ */
+function activate(app: JupyterFrontEnd) {
+  app.docRegistry.addWidgetExtension('Notebook', new ButtonExtension());
+};
+
+
+/**
+ * Export the plugin as default.
+ */
+export default plugin;
