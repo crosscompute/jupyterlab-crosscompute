4f5f480f2932cdf37f4033c20d30247322d42005
Roy Hyunjin Han <rhh@crosscompute.com>
2020-10-11 10:59:33 -0400

    Work from experiments by Rodrigo

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { requestAPI } from './crosscompute-jupyterlab-extensions';
+
+/**
+ * Initialization data for the crosscompute-jupyterlab-extensions extension.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'crosscompute-jupyterlab-extensions',
+  autoStart: true,
+  activate: (app: JupyterFrontEnd) => {
+    console.log('JupyterLab extension crosscompute-jupyterlab-extensions is activated!');
+
+    requestAPI<any>('get_example')
+      .then(data => {
+        console.log(data);
+      })
+      .catch(reason => {
+        console.error(
+          `The crosscompute_jupyterlab_extensions server extension appears to be missing.\n${reason}`
+        );
+      });
+  }
+};
+
+export default extension;
