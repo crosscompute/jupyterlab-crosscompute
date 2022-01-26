53e153d6415b76c4b20499d4e7977856851776d2
Roy Hyunjin Han <rhh@crosscompute.com>
2022-01-25 19:10:47 -0500

    Restart from scratch

---
+++ jupyterlab_crosscompute/src/index.ts
@@ -0,0 +1,43 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+
+import { requestAPI } from './handler';
+
+/**
+ * Initialization data for the jupyterlab-crosscompute extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab-crosscompute:plugin',
+  autoStart: true,
+  optional: [ISettingRegistry],
+  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
+    console.log('JupyterLab extension jupyterlab-crosscompute is activated!');
+
+    if (settingRegistry) {
+      settingRegistry
+        .load(plugin.id)
+        .then(settings => {
+          console.log('jupyterlab-crosscompute settings loaded:', settings.composite);
+        })
+        .catch(reason => {
+          console.error('Failed to load settings for jupyterlab-crosscompute.', reason);
+        });
+    }
+
+    requestAPI<any>('get_example')
+      .then(data => {
+        console.log(data);
+      })
+      .catch(reason => {
+        console.error(
+          `The jupyterlab_crosscompute server extension appears to be missing.\n${reason}`
+        );
+      });
+  }
+};
+
+export default plugin;
