908a9821de05fa4b5fc3dcdb260b4f5ce7dbe551
Roy Hyunjin Han <rhh@crosscompute.com>
2022-01-27 13:32:05 -0500

    Add empty sidebar with icon

--- jupyterlab_crosscompute/src/index.ts
+++ jupyterlab_crosscompute/src/index.ts
@@ -5,7 +5,10 @@ import {
 
 import { ICommandPalette } from '@jupyterlab/apputils';
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
+import { LabIcon } from '@jupyterlab/ui-components';
+import { Widget } from '@lumino/widgets';
 
+import logoSvgstr from '../style/icons/Logo-SmallFormat-20220127.svg';
 import { requestAPI } from './handler';
 
 /**
@@ -20,34 +23,41 @@ const plugin: JupyterFrontEndPlugin<void> = {
     palette: ICommandPalette | null,
     settingRegistry: ISettingRegistry | null
   ) => {
-    const { commands } = app;
+    const { commands, shell } = app;
 
-    const LAUNCH_COMMAND = 'crosscompute:launch';
-    const RENDER_COMMAND = 'crosscompute:render';
-    const DEPLOY_COMMAND = 'crosscompute:deploy';
+    const NAMESPACE = 'crosscompute';
+    const LAUNCH_COMMAND = NAMESPACE + ':launch';
+    const RENDER_COMMAND = NAMESPACE + ':render';
+    const DEPLOY_COMMAND = NAMESPACE + ':deploy';
 
     commands.addCommand(LAUNCH_COMMAND, {
       label: 'Launch Automation',
       execute: (args: any) => {
         console.log(LAUNCH_COMMAND, args['origin']);
-      },
+      }
     });
     commands.addCommand(RENDER_COMMAND, {
       label: 'Render Automation',
       execute: (args: any) => {
         console.log(RENDER_COMMAND, args['origin']);
-      },
+      }
     });
     commands.addCommand(DEPLOY_COMMAND, {
       label: 'Deploy Automation',
       execute: (args: any) => {
         console.log(DEPLOY_COMMAND, args['origin']);
-      },
+      }
     });
 
-    commands.execute(LAUNCH_COMMAND, { origin: 'init' }).catch((reason) => {
-      console.error('hey', reason);
+    const widget = new Widget();
+    const logoIcon = new LabIcon({
+      name: NAMESPACE + 'logo',
+      svgstr: logoSvgstr
     });
+    widget.title.icon = logoIcon;
+    widget.title.caption = 'CrossCompute';
+    widget.id = NAMESPACE + '-sidebar';
+    shell.add(widget, 'right', { rank: 1000 });
 
     if (palette) {
       const COMMAND_PALETTE_CATEGORY = 'CrossCompute';
@@ -55,19 +65,19 @@ const plugin: JupyterFrontEndPlugin<void> = {
       palette.addItem({
         command: LAUNCH_COMMAND,
         category: COMMAND_PALETTE_CATEGORY,
-        args: { origin: 'from palette' },
+        args: { origin: 'from palette' }
       });
 
       palette.addItem({
         command: RENDER_COMMAND,
         category: COMMAND_PALETTE_CATEGORY,
-        args: { origin: 'from palette' },
+        args: { origin: 'from palette' }
       });
 
       palette.addItem({
         command: DEPLOY_COMMAND,
         category: COMMAND_PALETTE_CATEGORY,
-        args: { origin: 'from palette' },
+        args: { origin: 'from palette' }
       });
     }
 
@@ -75,10 +85,16 @@ const plugin: JupyterFrontEndPlugin<void> = {
       settingRegistry
         .load(plugin.id)
         .then(settings => {
-          console.log('jupyterlab-crosscompute settings loaded:', settings.composite);
+          console.log(
+            'jupyterlab-crosscompute settings loaded:',
+            settings.composite
+          );
         })
         .catch(reason => {
-          console.error('Failed to load settings for jupyterlab-crosscompute.', reason);
+          console.error(
+            'Failed to load settings for jupyterlab-crosscompute.',
+            reason
+          );
         });
     }
 
