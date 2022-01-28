2b335799bac8a0a90e14d7ee545901a3a573e7dd
Roy Hyunjin Han <rhh@crosscompute.com>
2022-01-27 23:48:47 -0500

    Connect switches to commands

--- jupyterlab_crosscompute/src/index.ts
+++ jupyterlab_crosscompute/src/index.ts
@@ -8,6 +8,7 @@ import { ICommandPalette } from '@jupyterlab/apputils';
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
 import { ITranslator, nullTranslator } from '@jupyterlab/translation';
 import { LabIcon, Switch } from '@jupyterlab/ui-components';
+import { CommandRegistry } from '@lumino/commands';
 import { PanelLayout, Widget } from '@lumino/widgets';
 
 import logoSvgstr from '../style/icons/Logo-SmallFormat-20220127.svg';
@@ -19,9 +20,15 @@ const MAIN_PANEL_ID = NAMESPACE + '-main-panel';
 const MAIN_PANEL_CLASSNAME = NAMESPACE + '-MainPanel';
 const MAIN_PANEL_CAPTION = NAME;
 const LAUNCH_COMMAND = NAMESPACE + ':launch';
+const START_LAUNCH_COMMAND = LAUNCH_COMMAND + ':start';
+const STOP_LAUNCH_COMMAND = LAUNCH_COMMAND + ':stop';
 const RENDER_COMMAND = NAMESPACE + ':render';
+const START_RENDER_COMMAND = RENDER_COMMAND + ':start';
+const STOP_RENDER_COMMAND = RENDER_COMMAND + ':stop';
 const DEPLOY_COMMAND = NAMESPACE + ':deploy';
-const COMMAND_PALETTE_CATEGORY = 'CrossCompute';
+const START_DEPLOY_COMMAND = DEPLOY_COMMAND + ':start';
+const STOP_DEPLOY_COMMAND = DEPLOY_COMMAND + ':stop';
+// const COMMAND_PALETTE_CATEGORY = 'CrossCompute';
 
 const logoIcon = new LabIcon({
   name: NAMESPACE + ':logo',
@@ -43,28 +50,47 @@ const plugin: JupyterFrontEndPlugin<void> = {
     const { commands, shell } = app;
     const trans = translator.load('jupyterlab');
 
-    commands.addCommand(LAUNCH_COMMAND, {
-      label: trans.__('Launch Automation'),
+    commands.addCommand(START_LAUNCH_COMMAND, {
+      label: trans.__('Start Launch Automation'),
       execute: (args: any) => {
-        console.log(LAUNCH_COMMAND);
+        console.log(START_LAUNCH_COMMAND);
       }
     });
-    commands.addCommand(RENDER_COMMAND, {
-      label: trans.__('Render Automation'),
+    commands.addCommand(STOP_LAUNCH_COMMAND, {
+      label: trans.__('Stop Launch Automation'),
       execute: (args: any) => {
-        console.log(RENDER_COMMAND);
+        console.log(STOP_LAUNCH_COMMAND);
       }
     });
-    commands.addCommand(DEPLOY_COMMAND, {
-      label: trans.__('Deploy Automation'),
+    commands.addCommand(START_RENDER_COMMAND, {
+      label: trans.__('Start Render Automation'),
       execute: (args: any) => {
-        console.log(DEPLOY_COMMAND);
+        console.log(START_RENDER_COMMAND);
+      }
+    });
+    commands.addCommand(STOP_RENDER_COMMAND, {
+      label: trans.__('Stop Render Automation'),
+      execute: (args: any) => {
+        console.log(STOP_RENDER_COMMAND);
+      }
+    });
+    commands.addCommand(START_DEPLOY_COMMAND, {
+      label: trans.__('Start Deploy Automation'),
+      execute: (args: any) => {
+        console.log(START_DEPLOY_COMMAND);
+      }
+    });
+    commands.addCommand(STOP_DEPLOY_COMMAND, {
+      label: trans.__('Stop Deploy Automation'),
+      execute: (args: any) => {
+        console.log(STOP_DEPLOY_COMMAND);
       }
     });
 
-    const mainPanel = new MainPanel({ translator });
+    const mainPanel = new MainPanel({ commands, translator });
     shell.add(mainPanel, 'right', { rank: 1000 });
 
+    /*
     if (palette) {
       palette.addItem({
         command: LAUNCH_COMMAND,
@@ -81,6 +107,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
         category: COMMAND_PALETTE_CATEGORY
       });
     }
+    */
 
     if (settingRegistry) {
       settingRegistry
@@ -117,19 +144,25 @@ class MainPanel extends Widget {
     title.icon = logoIcon;
     title.caption = MAIN_PANEL_CAPTION;
 
+    const { commands } = options;
     const translator = options.translator || nullTranslator;
     const trans = translator.load('jupyterlab');
 
     const automationLaunchSwitch = new Switch();
     const automationRenderSwitch = new Switch();
     const automationDeploySwitch = new Switch();
-
-    /*
-    automationRenderSwitch.valueChanged.connect(_, args) => {
-      console.log('Launch');
+    automationLaunchSwitch.valueChanged.connect((_, args) => {
+      commands.execute(
+        args.newValue ? START_LAUNCH_COMMAND: STOP_LAUNCH_COMMAND);
     });
-    */
-
+    automationRenderSwitch.valueChanged.connect((_, args) => {
+      commands.execute(
+        args.newValue ? START_RENDER_COMMAND : STOP_RENDER_COMMAND);
+    });
+    automationDeploySwitch.valueChanged.connect((_, args) => {
+      commands.execute(
+        args.newValue ? START_DEPLOY_COMMAND : STOP_DEPLOY_COMMAND);
+    });
     automationLaunchSwitch.label = trans.__('Launch');
     automationRenderSwitch.label = trans.__('Render');
     automationDeploySwitch.label = trans.__('Deploy');
@@ -144,6 +177,7 @@ class MainPanel extends Widget {
 
 namespace MainPanel {
   export interface IOptions {
+    commands: CommandRegistry;
     translator: ITranslator;
   }
 }
