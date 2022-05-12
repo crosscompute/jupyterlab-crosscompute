3ec886dabfd850180e063fb24280d77e30c92dcd
Roy Hyunjin Han <rhh@crosscompute.com>
2022-05-12 07:47:55 -0400

    Revert environment variable

--- jupyterlab_crosscompute/constants.py
+++ jupyterlab_crosscompute/constants.py
@@ -3,5 +3,5 @@ from os import getenv
 
 NAMESPACE = 'jupyterlab-crosscompute'
 ID_LENGTH = 16
-BASE_URI = getenv('CROSSCOMPUTE_LAUNCH_BASE_URI', '')
+BASE_URI = getenv('CROSSCOMPUTE_BASE_URI', '')
 PROXY_URI = 'http://localhost:6000/api/routes'
