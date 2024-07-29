2fd76a23f40fc4ee06723e100d00101f2e52b272
Tam Nguyen <rhh@crosscompute.com>
2024-07-29 12:27:07 -0400

    Find path when going up/down in filebrowser

--- versions/0.3/src/index.ts
+++ versions/0.3/src/index.ts
@@ -4,21 +4,37 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import {
+  IDefaultFileBrowser
+} from '@jupyterlab/filebrowser';
+
+
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-crosscompute:plugin',
   description: 'CrossCompute Extensions for JupyterLab',
   autoStart: true,
-  requires: [ILabShell],
-  activate: (app: JupyterFrontEnd, labShell: ILabShell) => {
+  requires: [ILabShell, IDefaultFileBrowser],
+  activate: (app: JupyterFrontEnd, labShell: ILabShell, defaultFileBrowser: IDefaultFileBrowser) => {
     console.log('app', app);
     // const { shell } = app;
 
+    // Change focus when user opens different files
     labShell.currentPathChanged.connect((sender, args) => {
       console.log('labShell.currentPathChanged');
       console.log('sender', sender);
       console.log('args', args);
     });
 
+    // Change path when user go up/down in left filebrowser
+    if (defaultFileBrowser)
+      {
+          void Promise.all([app.restored, defaultFileBrowser.model.restored]).then(() => {
+    	defaultFileBrowser.model.pathChanged.connect((sender, args) => {
+            console.log('Detect Path changed');
+            console.log('Path', defaultFileBrowser.model.path);
+    	});
+    });
+    }
     // labShell.activeChanged.connect(f);
     // labShell.currentChanged.connect(f);
     // labShell.layoutModified.connect(f);
