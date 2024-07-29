6889ed7a5e737570469c99fb1b72e3c8732dd332
Tam Nguyen <rhh@crosscompute.com>
2024-07-29 11:17:43 -0400

    Test out DefaultFileBrowser

--- versions/0.3/src/index.ts
+++ versions/0.3/src/index.ts
@@ -8,7 +8,7 @@ import { ISettingRegistry } from '@jupyterlab/settingregistry';
 
 import { requestAPI } from './handler';
 
-import { IFileBrowserFactory } from '@jupyterlab/filebrowser';
+import { IFileBrowserFactory, IDefaultFileBrowser } from '@jupyterlab/filebrowser';
 
 import { IDocumentManager } from '@jupyterlab/docmanager';
 
@@ -21,29 +21,29 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-crosscompute:plugin',
   description: 'CrossCompute Extensions for JupyterLab',
   autoStart: true,
-  requires: [IFileBrowserFactory, IDocumentManager, ILabShell],
+  requires: [IDefaultFileBrowser, IFileBrowserFactory, IDocumentManager, ILabShell],
   optional: [ISettingRegistry],
   activate: (
     app: JupyterFrontEnd,
     browserFactory: IFileBrowserFactory,
+    defaultFileBrowser: IDefaultFileBrowser,
     docManager: IDocumentManager,
     labShell: ILabShell,
     settingRegistry: ISettingRegistry | null
   ) => {
     const { shell } = app;
 
-    const browser = browserFactory.createFileBrowser('my-browser');
-    const browserModel = browser.model;
+    // const browser = browserFactory.createFileBrowser('my-browser');
+    // const browserModel = browser.model;
 
     const openFolder = (folder: string) => {
-      labShell.activateById(browser.id);
-      browserModel.cd(folder);
+      // labShell.activateById(browser.id);
+      // browserModel.cd(folder);
+        console.log('Hello');
     };
 
     const openPath = (path: string) => docManager.openOrReveal(path);
 
-
-    // console.log(browserFactory.tracker.currentWidget?.model.path)
     
     const exampleWidget: ExampleWidget = new ExampleWidget(
       openFolder,
@@ -64,20 +64,37 @@ const plugin: JupyterFrontEndPlugin<void> = {
     };
     */
 
-    const f = () => {
-      console.log('pathChanged');
-    };
+    console.log(defaultFileBrowser);
+    // void Promise.all([app.restored, defaultFileBrowser.model.restored]).then(() => {
+    // 	defaultFileBrowser.model.pathChanged.connect((sender, args) => {
+    //         console.log('Detect Path changed');
+    // 	});
+    // });
+
+    
+    // browserFactory.tracker.widgetAdded.connect((sender: any, widget: any) => {
+    //   // Notify the instance tracker if restore data needs to update.
+    //   widget.context.pathChanged.connect(() => {
+    //     // tracker.save(widget);
+    //       console.log('From widgetAdded, path changed');
+    //   });
+    //   // tracker.add(widget);
+    // });
+
+    // const f = () => {
+    //   console.log('pathChanged');
+    // };
     const g = () => {
       console.log('layoutModified');
       console.log(browserFactory.tracker.currentWidget?.model.path);
       console.log(browserFactory.tracker.currentWidget?.selectedItems().next().value);
     };
 
-    console.log('check browserFactory currentWidget', browserFactory.tracker.currentWidget);
+    // console.log('check browserFactory currentWidget', browserFactory.tracker.currentWidget);
 
     // browserModel.pathChanged.connect(refresh);
     // labShell.layoutModified.connect(refresh);
-    browserModel.pathChanged.connect(f);
+    // browserModel.pathChanged.connect(f);
     labShell.layoutModified.connect(g);
 
     shell.add(exampleWidget, 'right', { rank: 1000 });
