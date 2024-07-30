d4c336e2a67527f6e954c71f7ed5b7fb9a59f03a
Tam Nguyen <rhh@crosscompute.com>
2024-07-30 10:46:55 -0400

    Add Code to update Panel on file change

--- versions/0.3/src/body.tsx
+++ versions/0.3/src/body.tsx
@@ -1,9 +1,40 @@
+
 import { ReactWidget } from '@jupyterlab/apputils';
+import React from 'react';
+import { logoIcon } from './constant';
+
+const TestComponent = (): JSX.Element => {
+    return (
+        <div>
+        <p>Test Component</p>
+        </div>
+    )
+}
+
 
 export class CrossComputePanel extends ReactWidget {
+  private _curFile;
+  private _curDir;
+  constructor() {
+    super();
+    this.id = 'widget-id';
+    this.addClass('jp-react-widget');
+
+    const title = this.title;
+    title.icon = logoIcon;
+    this._curFile = '';
+    this._curDir = '';
+
+  }
   render(): JSX.Element {
-    return (
-      <div></div>
-    );
+    return <TestComponent />;
+  }
+
+  updatePath(curFile: string, curDir: string) {
+      this._curFile = curFile;
+      this._curDir = curDir;
+
+      console.log('Updated Path:', this._curFile, this._curDir);
   }
 }
+
