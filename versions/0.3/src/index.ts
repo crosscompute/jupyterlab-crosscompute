3a5222fd22bcee520ad5c61dd9bf33992100b8ce
Roy Hyunjin Han <rhh@crosscompute.com>
2024-07-30 10:28:08 -0400

    Clean code

--- versions/0.3/src/index.ts
+++ versions/0.3/src/index.ts
@@ -3,41 +3,43 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-
-import {
-  IDefaultFileBrowser
-} from '@jupyterlab/filebrowser';
-
+import { IDefaultFileBrowser } from '@jupyterlab/filebrowser';
+import { CrossComputePanel } from './body';
 
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-crosscompute:plugin',
   description: 'CrossCompute Extensions for JupyterLab',
   autoStart: true,
   requires: [ILabShell, IDefaultFileBrowser],
-  activate: (app: JupyterFrontEnd, labShell: ILabShell, defaultFileBrowser: IDefaultFileBrowser) => {
+  activate: (
+    app: JupyterFrontEnd,
+    labShell: ILabShell,
+    defaultFileBrowser: IDefaultFileBrowser
+  ) => {
     console.log('app', app);
-    // const { shell } = app;
+    const { shell } = app;
 
     // Change focus when user opens different files
     labShell.currentPathChanged.connect((sender, args) => {
-      console.log('labShell.currentPathChanged');
-      console.log('sender', sender);
-      console.log('args', args);
+      console.log('labShell.currentPathChanged', sender, args);
     });
 
     // Change path when user go up/down in left filebrowser
-    if (defaultFileBrowser)
-      {
-          void Promise.all([app.restored, defaultFileBrowser.model.restored]).then(() => {
-    	defaultFileBrowser.model.pathChanged.connect((sender, args) => {
-            console.log('Detect Path changed');
-            console.log('Path', defaultFileBrowser.model.path);
-    	});
-    });
+    if (defaultFileBrowser) {
+      // Promise.all([app.restored, defaultFileBrowser.model.restored]).then(() => {
+      defaultFileBrowser.model.pathChanged.connect((sender, args) => {
+        console.log(
+          'defaultFileBrowser pathChanged',
+          defaultFileBrowser.model.path
+        );
+      });
+      // });
     }
     // labShell.activeChanged.connect(f);
     // labShell.currentChanged.connect(f);
     // labShell.layoutModified.connect(f);
+    const panel: CrossComputePanel = new CrossComputePanel();
+    shell.add(panel, 'right', { rank: 700 });
   }
 };
 
